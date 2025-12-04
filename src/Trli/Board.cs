794e1d84c59876bc4180102b0a924f6fd1a3d661
Kevin Burgon <buburgo@gmail.com>
2025-12-04 06:37:51 -0700

    Creation of first command
    This commit creates a prototype of the first command, which fetches all
    the open boards belonging to the requesting member.

---
+++ src/Trli/Board.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Trli;
+
+public record Board
+{
+	[JsonPropertyName("id")]
+	public string Id { get; set; } = string.Empty;
+
+	[JsonPropertyName("idOrganization")]
+	public string OrganizationId { get; set; } = string.Empty;
+
+	[JsonPropertyName("name")]
+	public string Name { get; set; } = string.Empty;
+
+	[JsonPropertyName("desc")]
+	public string Description { get; set; } = string.Empty;
+
+	[JsonPropertyName("url")]
+	public string Url { get; set; } = string.Empty;
+
+	[JsonPropertyName("starred")]
+	public bool Starred { get; set; } = false;
+
+	[JsonPropertyName("shortUrl")]
+	public string ShortUrl { get; set; } = string.Empty;
+}
+
+public static class BoardExtensions
+{
+	public static string ToConsoleString(this IEnumerable<Board> boards)
+	{
+		var idCharLength = 25;
+		var idOrgCharLength = 25;
+		var nameCharLength = 20;
+		var descCharLength = 20;
+		var starredCharLength = 7;
+
+		StringBuilder builder = new();
+		builder.AppendPrintColumn("ID", idCharLength);
+		builder.AppendPrintColumn("ORGANIZATION_ID", idOrgCharLength);
+		builder.AppendPrintColumn("NAME", nameCharLength);
+		builder.AppendPrintColumn("DESCRIPTION", descCharLength);
+		builder.AppendPrintColumn("STARRED", starredCharLength);
+		builder.AppendLine();
+
+		foreach (var board in boards)
+		{
+			builder.AppendPrintColumn(board.Id, idCharLength);
+			builder.AppendPrintColumn(board.OrganizationId, idOrgCharLength);
+			builder.AppendPrintColumn(board.Name, nameCharLength);
+			builder.AppendPrintColumn(board.Description, descCharLength);
+			builder.AppendPrintColumn(board.Starred, starredCharLength);
+			builder.AppendLine();
+		}
+
+		return builder.ToString();
+	}
+
+	public static void AppendPrintColumn(this StringBuilder builder, object value, int colMaxCharLength)
+	{
+		var valueText = value?.ToString() ?? string.Empty;
+		if (valueText.Length > colMaxCharLength)
+		{
+			builder.Append(valueText.AsSpan(0, colMaxCharLength));
+		}
+		else if (valueText.Length < colMaxCharLength)
+		{
+			builder.Append(valueText);
+			var extraChars = colMaxCharLength - valueText.Length;
+			for (int i = 0; i < extraChars; i++)
+			{
+				builder.Append(' ');
+			}
+		}
+		else
+		{
+			builder.Append(valueText);
+		}
+
+		builder.Append(' ');
+	}
+}
---
+++ src/Trli/Program.cs
@@ -0,0 +1,45 @@
+﻿using Cocona;
+using Flurl;
+using Flurl.Http;
+using Trli;
+const string ApiKeyEnvName = "TRELLO_API_KEY";
+const string TokenEnvName = "TRELLO_TOKEN";
+const string TrelloBaseApi = "https://api.trello.com/1/";
+
+await CoconaApp.RunAsync(async ([Argument] string command) =>
+{
+	if (command != "board")
+	{
+		Console.WriteLine($"ERROR: Command {command} invalid.");
+		return;
+	}
+
+    var apiKey = Environment.GetEnvironmentVariable(ApiKeyEnvName);
+    var apiToken = Environment.GetEnvironmentVariable(TokenEnvName);
+
+	if (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(apiToken))
+	{
+		Console.WriteLine($"ERROR: Empty API environment variable(s) found.  Please ensure that the environment variables {ApiKeyEnvName} and {TokenEnvName} are defined in the host environment.");
+		return;
+	}
+
+	var results = await TrelloBaseApi
+		.AppendPathSegment("members/me/boards")
+		.SetQueryParams(new Dictionary<string, string>
+			{
+				{ "key", apiKey },
+				{ "token", apiToken },
+				{ "filter", "open" }
+			})
+		.WithHeader("Accept", "application/json")
+		.GetJsonAsync<List<Board>>();
+
+	if (results == null)
+	{
+		Console.WriteLine("ERROR: Failed to get results from API.");
+		return;
+	}
+	
+	Console.WriteLine(results.ToConsoleString());
+});
+
